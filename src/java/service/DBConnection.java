9891f14d359e70474064bbf4edc6ebc6aeda00f4
AminSafari207 <safarin207368@gmail.com>
2025-08-02 10:43:54 +0330

    Implement db connection service using hikariCP

---
+++ src/java/service/DBConnection.java
@@ -0,0 +1,27 @@
+package service;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/student_course_management");
+        config.setUsername("postgres");
+        config.setPassword("postgrace");
+        config.setMaximumPoolSize(5);
+        config.setConnectionTimeout(10000);
+        config.setIdleTimeout(60000);
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
