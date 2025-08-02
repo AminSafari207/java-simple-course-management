cdf83e251ecf9f28eb19dbab2174f00bb0aeeb11
AminSafari207 <safarin207368@gmail.com>
2025-08-02 09:28:01 +0330

    Implement validation utils

---
+++ src/java/utils/ValidationUtils.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.util.List;
+
+public class ValidationUtils {
+    public static void validateId(int id) {
+        if (id < 0) throw new IllegalArgumentException("id must 0 or positive.");
+    }
+
+    public static void validateString(String logName, String str) {
+        if (str == null) throw new NullPointerException(logName + " can not be null.");
+        if (str.trim().isEmpty()) throw new IllegalArgumentException(logName + " can not be empty strings.");
+    }
+
+    public static void validateString(String logName, String str, int minLength) {
+        validateString(logName, str);
+        if (str.trim().length() < minLength) throw new IllegalArgumentException(logName + " must have at least " + minLength + " characters.");
+    }
+}
