8fcc2be380b4bb1e6a7f247a520187382df3ff61
AminSafari207 <safarin207368@gmail.com>
2025-08-05 11:19:56 +0330

    Implement truncateTables method for SqlUtils

---
+++ src/java/utils/SqlUtils.java
@@ -0,0 +1,29 @@
+package utils;
+
+import config.DBConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SqlUtils {
+    public static void truncateTables(String... tables) {
+        String sqlQuery = "truncate table ";
+
+        for (int i = 0; i < tables.length; i++) {
+            sqlQuery += (tables[i]);
+            if (i < tables.length - 1) sqlQuery += (", ");
+        }
+
+        sqlQuery += (" restart identity cascade");
+
+        try (
+                Connection connection = DBConnection.getConnection();
+                Statement statement = connection.createStatement()
+        ) {
+            statement.executeUpdate(sqlQuery);
+        } catch (SQLException e) {
+            throw new RuntimeException("Truncate error: " + e);
+        }
+    }
+}
